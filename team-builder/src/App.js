84c5280df916d17fca4af17ede20680cdad4fe62
Steven Sussman <sjsussman@gmail.com>
2020-09-15 15:37:07 -0400

    completed mvp

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { render } from 'react-dom'
 import './App.css';
+import Member from './Member'
+import Form from './Form'
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: '',
+}
 
 function App() {
+
+  const [teamMembers, setTeamMembers] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const updateForm = (inputName, inputValue) =>{
+  
+    setFormValues({...formValues, [inputName]: inputValue })
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+
+    if (!newMember.name || !newMember.name || !newMember.role){
+      return
+    }
+
+    setTeamMembers(teamMembers.concat(newMember))
+    setFormValues(initialFormValues)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Team Form</h1>
+
+      <Form 
+        values = {formValues}
+        update = {updateForm}
+        submit = {submitForm}
+        />
+
+    {
+      teamMembers.map(member => {
+        return (
+          <Member key = {member.id} details={member} />
+        )
+      })
+    }
     </div>
-  );
+
+  )
 }
+
+render (
+  <App />, document.querySelector('#root')
+)
 
 export default App;
